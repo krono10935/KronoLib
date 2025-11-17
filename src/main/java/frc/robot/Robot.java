b74ffefe6664e70e55bedf986d0e3b712ccabb29
Guykal221188 <guykalush.you@gmail.com>
2025-11-17 15:54:46 +0200

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,117 @@
+// Copyright (c) FIRST and other WPILib contributors.
+
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import io.github.captainsoccer.basicmotor.motorManager.MotorManager;
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+
+public class Robot extends LoggedRobot
+{
+    private Command autonomousCommand;
+    
+    private final RobotContainer robotContainer;
+    
+    
+    public Robot()
+    {
+
+        Logger.recordMetadata("ProjectName", "*GENERIC_ROBOT_PROJECT*"); // Set a metadata value
+
+        if (isReal()) {
+            Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+            Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+        } else {
+            setUseTiming(false); // Run as fast as possible
+            String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+            Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+            Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+        }
+
+        Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+
+        robotContainer = new RobotContainer();
+    }
+
+
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+        MotorManager.getInstance().periodic(); // must run AFTER CommandScheduler
+    }
+    
+    
+    @Override
+    public void disabledInit() {}
+    
+    
+    @Override
+    public void disabledPeriodic() {}
+    
+    
+    @Override
+    public void disabledExit() {}
+    
+    
+    @Override
+    public void autonomousInit()
+    {
+        autonomousCommand = robotContainer.getAutonomousCommand();
+        
+        if (autonomousCommand != null)
+        {
+            autonomousCommand.schedule();
+        }
+    }
+    
+    
+    @Override
+    public void autonomousPeriodic() {}
+    
+    
+    @Override
+    public void autonomousExit() {}
+    
+    
+    @Override
+    public void teleopInit()
+    {
+        if (autonomousCommand != null)
+        {
+            autonomousCommand.cancel();
+        }
+    }
+    
+    
+    @Override
+    public void teleopPeriodic() {}
+    
+    
+    @Override
+    public void teleopExit() {}
+    
+    
+    @Override
+    public void testInit()
+    {
+        CommandScheduler.getInstance().cancelAll();
+    }
+    
+    
+    @Override
+    public void testPeriodic() {}
+    
+    
+    @Override
+    public void testExit() {}
+}
