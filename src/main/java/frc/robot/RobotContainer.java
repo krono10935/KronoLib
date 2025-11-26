6253110fc0032abf806b1b12ca604032974aa882
Adam Cohen <municohen@gmail.com>
2025-11-26 20:38:07 +0200

    feat(drivetrain): Initialize Swerve drivetrain and basic auto command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,24 +5,33 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.swerve.SwerveDrivetrain;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-
+import frc.robot.commands.auto.DriveToPoseCommand;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.swerve.Swerve;
 
 
 public class RobotContainer
 {
+    private final Drivetrain drivetrain = new Swerve(() -> false);
     public RobotContainer()
     {
         configureBindings();
     }
     
     
-    private void configureBindings() {}
+    private void configureBindings() {
+//        drivetrain.setDefaultCommand(new DriveToPoseCommand(drivetrain, () -> new Pose2d()));
+    }
     
     
     public Command getAutonomousCommand()
     {
-        return Commands.print("No autonomous command configured");
+        return new DriveToPoseCommand(drivetrain, () -> new Pose2d(8,5, Rotation2d.fromDegrees(45)));
     }
 }
