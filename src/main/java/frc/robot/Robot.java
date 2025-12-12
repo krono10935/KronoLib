41901ae363e09550d8459bd69c02f8bd88a15484
Guykal221188 <guykalush.you@gmail.com>
2025-12-12 20:02:59 +0200

    added singleton structure to container

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -54,7 +54,7 @@ public class Robot extends LoggedRobot
 
         Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
 
-        robotContainer = new RobotContainer();
+        robotContainer = RobotContainer.getInstance();
     }
 
 
