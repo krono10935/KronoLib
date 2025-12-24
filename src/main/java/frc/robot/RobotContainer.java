6cfdf50c984eae9014b0fb0a89704f25cb23ae60
Adam Cohen <municohen@gmail.com>
2025-12-24 19:17:20 +0200

    changed the PathPlannerLib to my own one and added java doc

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,37 +5,34 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.swerve.SwerveDrivetrain;
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
-import com.pathplanner.lib.path.PathPlannerPath;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import com.pathplanner.lib.path.DriveToPose;
+import com.pathplanner.lib.path.DriveToPoseConstants;
 import edu.wpi.first.wpilibj2.command.*;
-import frc.robot.commands.auto.DriveToPoseCommand;
 import frc.robot.subsystems.drivetrain.Drivetrain;
-import frc.robot.subsystems.drivetrain.DrivetrainConstants;
 import frc.robot.subsystems.drivetrain.swerve.Swerve;
-import org.json.simple.parser.ParseException;
 import org.littletonrobotics.junction.Logger;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 
 public class RobotContainer
 {
 
-    private final Drivetrain drivetrain = new Swerve(() -> false);
+    private final LoggedDashboardChooser<Command> chooser;
+    private final Drivetrain drivetrain;
     public RobotContainer()
     {
+        drivetrain  = new Swerve(() -> false);
+        DriveToPose.configure(
+                new DriveToPoseConstants(
+                        drivetrain::getEstimatedPosition,
+                        drivetrain::getChassisSpeeds,
+                        drivetrain::drive,
+                        true
+                )
+        );
         configureBindings();
+        chooser = new LoggedDashboardChooser<>("chooser", AutoBuilder.buildAutoChooser());
     }
     
     
@@ -46,55 +43,12 @@ public class RobotContainer
 
 
 
-
-    }
-
-    public PathPlannerAuto createAutoFromPaths(PathPlannerPath[] paths){
-        return new PathPlannerAuto(createSequenceWithDriveCommand(paths));
-    }
-
-    public Command createSequenceWithDriveCommand(PathPlannerPath[] paths) {
-
-        SequentialCommandGroup auto = new SequentialCommandGroup();
-
-        for (PathPlannerPath p: paths){
-            Rotation2d finalRotation = p.getAllPathPoints().getLast().rotationTarget.rotation();
-            Translation2d finalTranslation = p.getAllPathPoints().getLast().position;
-            Pose2d finalPose = new Pose2d(finalTranslation.getX(), finalTranslation.getY(), finalRotation);
-
-
-
-            Logger.recordOutput("auto/isCloseEnough" + p.name, (BooleanSupplier) () -> drivetrain.getEstimatedPosition().getTranslation().getDistance(finalTranslation)<
-                    DrivetrainConstants.DISTANCE_TO_STOP_PP);
-            Logger.recordOutput("auto/isCloseEnoughDis" + p.name, (DoubleSupplier) ()-> drivetrain.getEstimatedPosition().getTranslation().getDistance(finalTranslation));
-            Logger.recordOutput("auto/finalTranslation" + p.name, finalPose);
-
-
-            auto.addCommands(AutoBuilder.followPath(p).until(() -> drivetrain.getEstimatedPosition().getTranslation().getDistance(finalTranslation)<
-                    DrivetrainConstants.DISTANCE_TO_STOP_PP));
-            auto.addCommands((new DriveToPoseCommand(drivetrain, ()-> finalPose)));
-
-
-        }
-        return auto;
-
     }
 
 
     public Command getAutonomousCommand()
     {
-        Command auto = null;
-        try {
-            PathPlannerPath a = PathPlannerPath.fromPathFile("a");
-            PathPlannerPath b = PathPlannerPath.fromPathFile("b");
-            PathPlannerPath[] paths = {a,b};
-            auto = createSequenceWithDriveCommand(paths);
-
-        } catch (IOException | ParseException e){
-            e.getStackTrace();
-        }
-
-        return auto;
+        return chooser.get();
     }
 
 
