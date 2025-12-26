6f40bf36ef861dbe71e8496f24dfa6c41c0b7b35
guykl2 <guykalush.you@gmail.com>
2025-12-26 13:49:03 +0200

    Ledline IDS and javadocs
    Added capability to specify the ledline in the location enum and added javadocs where they were missing.

--- src/main/java/frc/robot/leds/LedLocation.java
+++ src/main/java/frc/robot/leds/LedLocation.java
@@ -1,14 +1,16 @@
 package frc.robot.leds;
 
 public enum LedLocation{
-    BASE(0,1),
-    ARM(1,2),
-    END_EFFECTOR(2,3);
+    BASE(0,0,1),
+    ARM(1,1,2),
+    END_EFFECTOR(2,2,3);
 
+    public final int ledLineID;
     public final int start;
     public final int end;
 
-    LedLocation(int start, int end){
+    LedLocation(int ledLineID,int start, int end){
+        this.ledLineID = ledLineID;
         this.start = start;
         this.end = end;
     }
