c3a41960e76fafe818cff9be0a9ee915b4b0baab
guykl2 <guykalush.you@gmail.com>
2025-12-26 15:51:02 +0200

    Cleaned up and moved things around

--- src/main/java/frc/utils/ErrorMessage.java
+++ src/main/java/frc/utils/ErrorMessage.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.lib;
+package frc.utils;
 
 import java.util.function.BooleanSupplier;
 
@@ -12,24 +12,16 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * Error message class.
- * @param subsystem the subsystem which is sending the error
- * @param code the error code from the subsystem
- * @param message a message connected to the error
- * @param shouldDisplayError boolean supplier trigger for the error
- * @param onTrue a runnable to run once the supplier returns true
- * @param onFalse a runnable to run once the supplier returns true
- * @param alert an Alert object which is turned on once the trigger activates
+ * Represent an Error message and the relevant action connected to it
  */
-public record ErrorMessage(
-    Subsystem subsystem, 
-    int code, 
-    String message, 
-    BooleanSupplier shouldDisplayError,
-    Runnable onTrue,
-    Runnable onFalse,
-    Alert alert
-    ) {
+public class ErrorMessage{
+    private Subsystem subsystem;
+    private int code;
+    private String message;
+    private BooleanSupplier shouldDisplayError;
+    private Runnable onTrue;
+    private Runnable onFalse;
+    private Alert alert;
     /**
      * @param subsystem the subsystem which is sending the error
      * @param code the error code from the subsystem
@@ -41,16 +33,21 @@ public record ErrorMessage(
     public ErrorMessage(Subsystem subsystem, int code, String message,
      BooleanSupplier shouldDisplayError, Runnable onTrue, Runnable onFalse) {
 
-        this(subsystem, code, message, shouldDisplayError, onTrue, onFalse, new Alert(
-            "Error in " + subsystem.getName() + " | Code: " + code + " | Message: " + message,
-            Alert.AlertType.kError
-        ));
+        this.subsystem = subsystem;
+        this.code = code;
+        this.message = message;
+        this.shouldDisplayError = shouldDisplayError;
+        this.onTrue = onTrue;
+        this.onFalse = onFalse;
+        this.alert = new Alert(
+                "Error in " + subsystem.getName() + " | Code: " + code + " | Message: " + message,
+                Alert.AlertType.kError
+        );
 
         new Trigger(shouldDisplayError).onTrue(
                 new InstantCommand(() -> alert.set(true))
-                        .andThen(new InstantCommand(() -> onTrue.run())));
-
-        new Trigger(shouldDisplayError).onFalse(
+                        .andThen(new InstantCommand(() -> onTrue.run())))
+                .onFalse(
                 new InstantCommand(() -> alert.set(false))
                         .andThen(new InstantCommand(() -> onFalse.run())));
     }
