8deca98b4794343e93be9c166f69201c4389cb4d
guykl2 <guykalush.you@gmail.com>
2026-01-02 12:40:08 +0200

    build constants

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "KronoLib";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 18;
+  public static final String GIT_SHA = "933e65bc71ee7bfff84a44a903c272ced2d9d8c2";
+  public static final String GIT_DATE = "22-12-2025 19:42:26 IST";
+  public static final String GIT_BRANCH = "SwerveSetPointGenerator";
+  public static final String BUILD_DATE = "31-12-2025 20:06:40 IST";
+  public static final long BUILD_UNIX_TIME = 1767204400504L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
