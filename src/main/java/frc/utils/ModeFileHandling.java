8d41ef1d2aef7193476bc1a552564fe5fa604b39
Guykal221188 <guykalush.you@gmail.com>
2026-01-03 23:56:08 +0200

    removed dumb check, updated the bash script and added check for if unable to delete file

--- src/main/java/frc/utils/ModeFileHandling.java
+++ src/main/java/frc/utils/ModeFileHandling.java
@@ -3,6 +3,7 @@ package frc.utils;
 import edu.wpi.first.wpilibj.DriverStation;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class ModeFileHandling {
     private static boolean fmsAttached = false;
@@ -21,7 +22,7 @@ public class ModeFileHandling {
      */
     public static void switchToPitMode() {
         File file = new File(MODE_FILE_PATH);
-        file.delete();
+        if(!file.delete()) throw new RuntimeException("Unable to delete pitmode file");
 
     }
 
